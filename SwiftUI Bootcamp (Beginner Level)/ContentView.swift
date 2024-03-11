8798684b397f610266166ff14d17ad915db454ee
jgestradam <jgestradam@unal.edu.co>
2024-03-11 13:41:54 -0500

    Initial Commit

---
+++ SwiftUI Bootcamp (Beginner Level)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Joseph Estrada on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Bootcamp (Beginner Level)/SwiftUI_Bootcamp__Beginner_Level_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bootcamp__Beginner_Level_App.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Joseph Estrada on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bootcamp__Beginner_Level_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
