f6bfdf9dd6daed7f240a2f952f7908d1b4b495eb
jgestradam <jgestradam@unal.edu.co>
2024-03-11 16:38:45 -0500

    How to use inits and enums in SwiftUI | Bootcamp #13

---
+++ SwiftUI Bootcamp (Beginner Level)/How to use inits and enums in SwiftUI | Bootcamp #13.swift
@@ -0,0 +1,18 @@
+//
+//  How to use inits and enums in SwiftUI | Bootcamp #13.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Joseph Estrada on 3/11/24.
+//
+
+import SwiftUI
+
+struct How_to_use_inits_and_enums_in_SwiftUI___Bootcamp__13: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    How_to_use_inits_and_enums_in_SwiftUI___Bootcamp__13()
+}
