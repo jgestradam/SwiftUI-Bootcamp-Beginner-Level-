b0e5ff4c6e1d64a560afba94c4ef967a352bdcdc
jgestradam <jgestradam@unal.edu.co>
2024-03-11 13:57:18 -0500

    Adding Text in SwiftUI | Bootcamp #2

---
+++ SwiftUI Bootcamp (Beginner Level)/Adding Text in SwiftUI | Bootcamp #2.swift
@@ -0,0 +1,18 @@
+//
+//  Adding Text in SwiftUI | Bootcamp #2.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Joseph Estrada on 3/11/24.
+//
+
+import SwiftUI
+
+struct Adding_Text_in_SwiftUI___Bootcamp__2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Adding_Text_in_SwiftUI___Bootcamp__2()
+}
