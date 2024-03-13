70444d913d41ddde7f13df31f224d196dbc6048d
jgestradam <jgestradam@unal.edu.co>
2024-03-13 13:12:48 -0500

    Add, edit, move, and delete items in a List in SwiftUI | Bootcamp #31

---
+++ SwiftUI Bootcamp (Beginner Level)/Add, edit, move, and delete items in a List in SwiftUI | Bootcamp #31.swift
@@ -0,0 +1,18 @@
+//
+//  Add, edit, move, and delete items in a List in SwiftUI | Bootcamp #31.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Joseph Estrada on 3/13/24.
+//
+
+import SwiftUI
+
+struct Add__edit__move__and_delete_items_in_a_List_in_SwiftUI___Bootcamp__31: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Add__edit__move__and_delete_items_in_a_List_in_SwiftUI___Bootcamp__31()
+}
